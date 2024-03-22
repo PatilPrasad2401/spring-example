38436bb820137ea4d6a7f2736d8986374d0f5da5
Prasad <patilprasad2401@gmail.com>
2024-03-22 15:28:28 +0530

    add spring hello-world example  programs..

--- src/main/java/com/prasad/practice/App.java
+++ src/main/java/com/prasad/practice/App.java
@@ -1,5 +1,7 @@
 package com.prasad.practice;
 
+import com.prasad.practice.model.Address;
+import com.prasad.practice.model.City;
 import com.prasad.practice.service.HelloWoldService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -7,11 +9,15 @@ public class App
 {
     public static void main( String[] args )
     {
-//        HelloWoldService helloWoldService=new HelloWoldService();
-//        helloWoldService.setName("Prasad");
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        HelloWoldService helloWoldService=new HelloWoldService();
+        helloWoldService.setName("Prasad");
+        City city=new City();
+        city.setName("pune");
+        Address address=new Address(city);
+        helloWoldService.setAddress(address);
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        HelloWoldService helloWoldService = (HelloWoldService) context.getBean("helloWorldService");
+//        HelloWoldService helloWoldService = (HelloWoldService) context.getBean("helloWorldService");
 //        HelloWoldService helloWoldService = context.getBean("helloService", HelloService.class);
 
         helloWoldService.sayHello();
